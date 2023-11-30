e410305c79f952cce25fba12bea9428d5ac57036
Yingzhe Dong <yzdong99@bu.edu>
2023-11-30 17:16:11 -0500

    update network container sh

--- docs/perf/micro_benchmark/network/network_container.sh
+++ docs/perf/micro_benchmark/network/network_container.sh
@@ -4,14 +4,18 @@
 
 # Define variables
 NETWORK_NAME="perf-kata-network"
-KATA1_CONTAINER="kata1-03"
-KATA1_CONTAINER_ENABLE="kata1-03-enable"
+KATA1_CONTAINER="kata1-network"
+KATA1_CONTAINER_ENABLE="kata1-network-enable"
 KATA1_IMAGE="perf-kata1-image"
 DEFAULT_RUNTIME="result_default.txt"
 KATA_RUNTIME="result_kata.txt"
+DOCKERFILE="DockerfileKata1"
+HOSTNAME=$(hostname)
 # KATA2_CONTAINER="kata2-6"
 # KATA2_IMAGE="perf-kata2-image"
 
+sudo docker build -t $KATA1_IMAGE -f $DOCKERFILE .
+sudo docker network create perf-kata-network
 echo "Starting the deployment of Docker containers with default runtime"
 
 # Run kata1 container with default runtime
@@ -26,7 +30,7 @@ sleep 15
 # Display logs from kata2 container
 # sudo docker logs $KATA2_CONTAINER
 
-iperf3 -c kata1 > $DEFAULT_RUNTIME
+iperf3 -c $HOSTNAME > $DEFAULT_RUNTIME
 
 echo "Deployment default runtime container complete."
 # Stop and remove the kata1 container
@@ -50,7 +54,7 @@ sleep 15
 # Display logs from kata2 container
 # sudo docker logs $KATA2_CONTAINER
 
-iperf3 -c kata1 > $KATA_RUNTIME
+iperf3 -c $HOSTNAME > $KATA_RUNTIME
 
 echo "Deployment kata runtime container complete."
 # Stop and remove the kata1 container
